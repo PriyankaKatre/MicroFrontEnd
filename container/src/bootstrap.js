78ad1d68f34deacd8964776648f68b826934a799
Priyanka Katre <priyanka.katre@greenmangaming.com>
2024-06-14 15:17:50 +0100

    MFE

--- container/src/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { mount as productMount} from 'products/ProductsIndex';
-import { mount as cartMount} from 'cart/cartShow'
-
-
-productMount(document.querySelector('#my-products'))
-cartMount(document.querySelector('#my-cart'))
