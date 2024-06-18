ec86647370e71eef7bf1c223d8d7c54f1337e020
Priyanka Katre <priyanka.katre@greenmangaming.com>
2024-06-18 15:15:50 +0100

    MFE routing

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp.js";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import Header from './components/Header.js'
+
+// This is for to avoid css namespace collesion when useing same framwork in MFE
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
-        <>
-            <MarketingApp />
-        </>
+        <BrowserRouter generateClassName={generateClassName}>
+            <StylesProvider>
+                <Header />
+                <MarketingApp />
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
--- mfp/packages/container/src/components/MarketingApp.js
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import { mount } from 'marketing/marketingApp'
 import React, { useRef, useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom';
+
 
 export default  () => {
     const ref = useRef(null);
-
+    const history = useHistory();
     useEffect(() => {
-        mount(ref.current)
+       const {onParentNavigate} =  mount(ref.current, {
+            onNavigate: ({ pathname: nextPathName }) => {
+                const { pathname } = history.location
+                if (pathname !== nextPathName) {
+                    history.push(nextPathName)
+                }
+
+            }
+       })
+
+        history.listen(onParentNavigate);
     },[])
 
     return (
